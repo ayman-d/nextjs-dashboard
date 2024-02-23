b353d5e8b8b74d114d5ed9ac74d67ee6fefd44e5
Ayman Al-Dali <aaldali@profireenergy.com>
2024-02-23 08:32:03 -0700

    tested using sql instead of supabase client

--- src/lib/actions/revenue-actions.ts
+++ src/lib/actions/revenue-actions.ts
@@ -4,6 +4,15 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import type { Database } from '@/database.types';
 import { Revenue } from '@/src/lib/types/revenue-types';
+import postgres from 'postgres';
+
+const sql = postgres({
+  host: 'aws-0-ca-central-1.pooler.supabase.com',
+  database: 'postgres',
+  port: 5432,
+  user: 'postgres.yhicazhgeuiszmdmcmoz',
+  password: '3or#S!#UKGK74s',
+});
 
 /**
  * @brief function to get revenue records from the database
@@ -24,6 +33,21 @@ export async function getRevenue(): Promise<Revenue[]> {
   if (error) {
     throw Error('Failed to fetch revenue data');
   }
+
+  const stuff = await sql`
+    SELECT id, amount FROM public.invoices limit 5;
+  `;
+
+  const stuff2 = await sql<Revenue[]>`
+  SELECT id, revenue FROM public.revenue limit 5;
+  `;
+
+  stuff2.map((item) => (item.revenue = Number(item.revenue)));
+
+  console.log(stuff);
+  console.log('brr');
+  console.log(stuff2);
+  console.log('zrr');
 
   // return the revenue data
   return data;
